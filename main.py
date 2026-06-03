826c8487b9cfdf1b364a5dbffcfcd6ade184400d
Gumbo-Stoic <sky.cry321@gmail.com>
2026-06-03 15:18:58 +0900

    created ship, made it move up down and rotate

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0.0
     clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         clock.tick(60)
         dt = clock.tick(60) / 1000.0
         pygame.display.flip()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+ 
 
 if __name__ == "__main__":
     main()
