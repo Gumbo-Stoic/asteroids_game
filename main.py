cc4127bdca2a32c619e0a298076029e5b5f174ef
Gumbo-Stoic <sky.cry321@gmail.com>
2026-06-05 16:49:10 +0900

    asteroids now split when shot

--- main.py
+++ main.py
@@ -33,6 +33,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+        for obj in asteroids:
+            for shot in shots:
+                if shot.collides_with(obj):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    obj.split()
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
